7ea0dd6e98fd06712c44428d64545a8ff96eb689
Giri Prasad P <giriprasa@gmail.com>
2025-08-14 13:44:18 +0530

    main updated a and b

---
+++ a.js
@@ -0,0 +1,3 @@
+console.log('This is a.js file');
+console.log('This is the middle of a.js file');
+console.log('This is the end of a.js file');
---
+++ b.js
@@ -0,0 +1,11 @@
+console.log('This is b.js file');
+console.log('This is the middle of b.js file');
+console.log('This is the end of b.js file');
+
+function add(a, b) {
+    console.log('Adding two numbers:', a, b);
+    console.log(a+b);
+    return a + b;
+}
+
+
